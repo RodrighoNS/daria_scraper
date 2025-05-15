df83a45af40a784b412e8d05def4dcf250d56ce2
RodrighoNS <rod.nvrr@gmail.com>
2025-05-15 02:29:12 -0400

    refactor: remove unused character attributes

--- daria_scraper/models/character.py
+++ daria_scraper/models/character.py
@@ -14,16 +14,6 @@ class Character:
         """
         self.url = url
         self.full_name = ""
-        self.current_age = ""
-        self.current_vocation = ""
-        self.season_one_age = ""
-        self.season_one_vocation = ""
-        self.parents = ""
-        self.siblings = ""
-        self.first_appearance = ""
-        self.status_at_end_of_series = ""
-        self.character_on_self = ""
-        self.description = []
         self.alter_egos_images = []
 
     def to_dict(self):
@@ -36,16 +26,6 @@ class Character:
         return {
             "url": self.url,
             "full_name": self.full_name,
-            "current_age": self.current_age,
-            "current_vocation": self.current_vocation,
-            "season_one_age": self.season_one_age,
-            "season_one_vocation": self.season_one_vocation,
-            "parents": self.parents,
-            "siblings": self.siblings,
-            "first_appearance": self.first_appearance,
-            "status_at_end_of_series": self.status_at_end_of_series,
-            "character_on_self": self.character_on_self,
-            "description": self.description,
             "alter_egos_images": self.alter_egos_images
         }
 
@@ -62,15 +42,5 @@ class Character:
         """
         character = cls(data.get("url", ""))
         character.full_name = data.get("full_name", "")
-        character.current_age = data.get("current_age", "")
-        character.current_vocation = data.get("current_vocation", "")
-        character.season_one_age = data.get("season_one_age", "")
-        character.season_one_vocation = data.get("season_one_vocation", "")
-        character.parents = data.get("parents", "")
-        character.siblings = data.get("siblings", "")
-        character.first_appearance = data.get("first_appearance", "")
-        character.status_at_end_of_series = data.get("status_at_end_of_series", "")
-        character.character_on_self = data.get("character_on_self", "")
-        character.description = data.get("description", [])
         character.alter_egos_images = data.get("alter_egos_images", [])
         return character
